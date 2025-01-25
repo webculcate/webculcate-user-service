67f8c40ff39f0ae79d32af116393f69e8211a58b
webculcate <webculcate@gmail.com>
2025-01-25 11:25:34 +0530

    added support for sd

--- user-service-core/src/main/java/com/webculcate/userservice/core/UserServiceCoreApplication.java
+++ user-service-core/src/main/java/com/webculcate/userservice/core/UserServiceCoreApplication.java
@@ -2,8 +2,10 @@ package com.webculcate.userservice.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UserServiceCoreApplication {
 
 	public static void main(String[] args) {
