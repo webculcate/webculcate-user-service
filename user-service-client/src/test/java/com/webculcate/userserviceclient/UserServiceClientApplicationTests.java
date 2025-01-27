f47806e77f60773127201b9a736b3f6e00a1c702
webculcate <webculcate@gmail.com>
2025-01-27 19:14:21 +0530

    added user service client

---
+++ user-service-client/src/test/java/com/webculcate/userserviceclient/UserServiceClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webculcate.userserviceclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
